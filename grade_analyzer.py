dca359383369a774866f13c4e8236f8c0538e758
mphvictrsepng-rgb <mphvoctrsepng@gmail.com>
2026-05-28 23:16:06 +0200

    my starter data

---
+++ grade_analyzer.py
@@ -0,0 +1,8 @@
+students = [
+    {"name": "Amara",  "marks": [78, 85, 90, 72]},
+    {"name": "Lerato", "marks": [55, 60, 48, 70]},
+    {"name": "Sipho",  "marks": [92, 88, 95, 91]},
+    {"name": "Fatima", "marks": [65, 70, 68, 75]},
+    {"name": "Kagiso", "marks": [40, 35, 50, 45]},
+]
+
